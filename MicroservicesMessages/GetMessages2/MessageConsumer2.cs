f8ac98fc47e6db93c2e4305998e8b7c8fde0e2e5
Dor <dorsharoni10@gmail.com>
2021-04-23 11:35:09 +0300

    Update MessageConsumer2.cs

--- MicroservicesMessages/GetMessages2/MessageConsumer2.cs
+++ MicroservicesMessages/GetMessages2/MessageConsumer2.cs
@@ -11,7 +11,7 @@ namespace GetMessages2
     {
         public async Task Consume(ConsumeContext<FilterMessage> context)
         {
-            await Console.Out.WriteLineAsync($"Recieved 2:\n Name: {context.Message.Name} Date: {context.Message.Date}, Job: {context.Message.Job}");
+            await Console.Out.WriteLineAsync($"Recieved 2:\n Name: {context.Message.Name} Date: {context.Message.Date}, Job: {context.Message.Job}")
         }
     }
 }
